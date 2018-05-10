5697a36e3c4591d61ab2ff9af88468a47310cae7
杨帆 <incomparable9527@foxmail.com>
2018-05-10 20:24:37 +0800

    配置自动化

---
+++ index.js
@@ -0,0 +1,34 @@
+const chokidar = require('chokidar');
+const fs = require('fs');
+const fse = require('fs-extra');
+const PROJECT_PATH = '/root/bidding/'
+
+//监听文件变化
+chokidar.watch(PROJECT_PATH, {
+    ignored: './node_modules',
+    awaitWriteFinish: {
+        pollInterval: 3000 
+    }
+}).on('all', (event, path) => {
+    // console.log(event, path);
+    let config = generateConfig(PROJECT_PATH,[]) 
+    console.log(config)
+    fs.writeFileSync(`${PROJECT_PATH}/project.config.js`, `module.exports={projects:${JSON.stringify(config)}}`, {
+        encoding: 'utf-8'
+      });
+    console.log('配置文件已经重新生成')
+}); 
+
+// 生成配置文件
+function generateConfig(path, config) {
+    var files = fs.readdirSync(path);
+    files.forEach((element)=>{
+      var stat = fs.statSync(path + '/' + element);
+      if (stat.isDirectory()) {
+        //递归读取文件
+        let json = fse.readJsonSync(path + '/' + element + '/project.json',{ throws: false });
+        if(json){ config.push(json); }
+      }
+    })
+    return config;
+  }
