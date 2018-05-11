76bcda4d543c98768f370d828aa16acb514bcf60
杨帆 <incomparable9527@foxmail.com>
2018-05-11 13:27:41 +0800

    优化

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const chokidar = require('chokidar');
 const fs = require('fs');
 const fse = require('fs-extra');
 const PROJECT_PATH = '.'
-
+console.log('配置自动化已经启动')
 //监听文件变化
 chokidar.watch(PROJECT_PATH, {
     ignored: /(^|[\/\\])\..|.git|project.config.js|node_modules/,
@@ -10,13 +10,12 @@ chokidar.watch(PROJECT_PATH, {
         pollInterval: 20000
     }
 }).on('all', (event, path) => {
-    // console.log(event, path);
+    console.log(event, path);
     let config = generateConfig(PROJECT_PATH,[]) 
-    console.log(config)
     fs.writeFileSync(`${PROJECT_PATH}/project.config.js`, `module.exports={projects:${JSON.stringify(config)}}`, {
         encoding: 'utf-8'
       });
-    console.log('配置文件已经重新生成')
+    console.log('配置文件已经重新生成',config)
 }); 
 
 // 生成配置文件
